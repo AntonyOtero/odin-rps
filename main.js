5bbbb5978abfb2e79f5cb3838bba65fda322bea1
Antony Otero <16374313+AntonyOtero@users.noreply.github.com>
2025-04-02 23:05:17 -0400

    Add base project files

---
+++ main.js
@@ -0,0 +1,6 @@
+// Get a random integer between min (inclusive) to max (exclusive)
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}  
